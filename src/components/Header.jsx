cfea7638e8f684ebe3f79df0e441989b5d745b54
sujal290 <117660164+sujal290@users.noreply.github.com>
2025-04-16 00:07:07 +0530

    day 1 part 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,30 +3,19 @@ import React from 'react';
 import styles from '../styles/Header/Header.module.css';
 
 import Container from './Container';
+// console.log("Header is rendering...");
+
  
 
 export default function Header() {  
   return (  
     <header className={`${styles.header} py-3 px-1 shadow`}>  
       <Container className="flex justify-between items-center">  
-        {/* Logo */}  
-        <Link href="/">  
+      
           <div className="flex items-center">  
-            <span className="text-pink-500 font-bold text-4xl">E-Store<b className="text-black">.</b></span>  
+            <span className="text-pink-500 font-bold text-4xl">E-Store
+              <b className="text-black">.</b></span>  
           </div>  
-        </Link>  
-
-        {/* Search Bar */}  
-        <div className={`${styles.searchBar} flex items-center`}>  
-          <input  
-            type="text"  
-            placeholder="Search for products..."  
-            className={styles.searchInput}  
-          />  
-          <button className={styles.searchButton}>  
-            <FiSearch size={18} />  
-          </button>  
-        </div>  
       </Container>  
     </header>  
   );  
